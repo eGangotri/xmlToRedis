87ccd65b548cac3a7c2aa24859c371cd7b706323
Chetan <chetan.pandey@isentia.com>
2021-12-05 18:11:37 +0800

    .

--- index.ts
+++ index.ts
@@ -2,29 +2,22 @@ import * as Redis from './service/redisService';
 import { xmlToJson } from './service/xmlService'
 
 async function xml2Redis(xmlAsJson: any) {
-    console.log(`XML to Redis`);
+    console.log(`Welcome to XML to Redis`);
     const configTuple = []
     configTuple.push(["subdomains", JSON.stringify(xmlAsJson.config.subdomains?.subdomain)])
     const cookieObj = xmlAsJson.config.cookies?.cookie;
-    const cookies = cookieObj.map((element: any) => {
+    cookieObj.map((element: any) => {
         configTuple.push([`cookie:${element.name}:${element.host}`, element.$t]);
     })
-    //await Redis.setValues(configTuple);
+    await Redis.setValues(configTuple);
 }
 
 async function readRedisValues() {
     const subdomains = await Redis.getValues("subdomains");
     const avgCookie = await Redis.getValues("cookie:dlp-avg:vector");
-
-    console.log(`subdomains ${subdomains}`);
-    console.log(`avgCookie ${avgCookie}`);
 }
 
 const xmlAsJson = xmlToJson('config.xml');
-console.log("***JSON ->" + JSON.stringify(xmlAsJson) + "***\n");
-console.log("subdomains ->", xmlAsJson.config.subdomains?.subdomain);
-console.log('-->' + JSON.stringify(xmlAsJson.config.cookies?.cookie));
 xml2Redis(xmlAsJson);
 readRedisValues();
-
-
+process.exit(0)
